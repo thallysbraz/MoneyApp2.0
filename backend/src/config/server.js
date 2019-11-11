3d018b8c4436e55a46029930824e2669da63a86f
thallysbraz <thallysbraz3@gmail.com>
2019-11-11 15:00:49 -0300

    finalizando backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,13 +1,18 @@
 const port = 3003;
 const express = require("express");
 const bodyParser = require("body-parser");
+const queryParser = require("express-query-int");
+
+const allowCors = require("./cors");
 
 const server = express();
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(allowCors);
+server.use(queryParser());
 
 server.listen(port, function() {
-  console.log("server rodando na porta: ", +port);
+  console.log("server rodando na porta:", +port);
 });
 
 module.exports = server;
