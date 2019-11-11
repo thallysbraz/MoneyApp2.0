6c8d380d8a567a4674c3949fd95c6d606c03aa1f
thallysbraz <thallysbraz3@gmail.com>
2019-11-11 16:02:54 -0300

    componente footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      <a href="https://github.com/thallysbraz" target="_blank">
+        Thallys Braz
+      </a>
+    </strong>
+  </footer>
+);
