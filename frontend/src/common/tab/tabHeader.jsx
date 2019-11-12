fd82d7c464d51debc3c4acb1e6ece0116c67fee1
thallysbraz <thallysbraz3@gmail.com>
2019-11-12 14:57:34 -0300

    Component TabHeader

--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -1,3 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
-export default props => <ul className="nav nav-tabs">{props.children}</ul>;
+import If from "../operator/if";
+import { selectTab } from "./tabActions";
+
+class TabHeader extends Component {
+  render() {
+    return (
+      <li>
+        <a
+          href="javascript:;"
+          data-toggle="tab"
+          data-target={this.props.target}
+        >
+          <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
+        </a>
+      </li>
+    );
+  }
+}
+
+export default TabHeader;
