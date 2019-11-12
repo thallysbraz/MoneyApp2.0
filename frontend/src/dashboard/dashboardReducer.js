f6cbf50a4efe5ef406d3e11edbca10aeae92e4f7
thallysbraz <thallysbraz3@gmail.com>
2019-11-12 14:01:34 -0300

    Dashboard com Redux 2 e conexão com backend

--- frontend/src/dashboard/dashboardReducer.js
+++ frontend/src/dashboard/dashboardReducer.js
@@ -1,5 +1,10 @@
 const INITAL_STATE = { summary: { credit: 0, debt: 0 } };
 
 export default function(state = INITAL_STATE, action) {
-  return state;
+  switch (action.type) {
+    case "BILLING_SUMMARY_FETCHED":
+      return { ...state, summary: action.payload.data };
+    default:
+      return state;
+  }
 }
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
+import promise from "redux-promise";
 
-import reducers from "./main/reducers";
 import App from "./main/app";
+import reducers from "./main/reducers";
 
-const store = createStore(reducers);
+const store = applyMiddleware(promise)(createStore)(reducers); //middleware
 
 ReactDOM.render(
   <Provider store={store}>
