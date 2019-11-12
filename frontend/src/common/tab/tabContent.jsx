250777fe9dff218d949aa979b5d694544aad9ae0
thallysbraz <thallysbraz3@gmail.com>
2019-11-12 15:38:50 -0300

    Componente tabContent

--- frontend/src/common/tab/tabContent.jsx
+++ frontend/src/common/tab/tabContent.jsx
@@ -1,3 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
-export default props => <div className="tab-content">{props.children}</div>;
+class TabContent extends Component {
+  render() {
+    const selected = this.props.tab.selected === this.props.id;
+    return (
+      <div
+        id={this.props.id}
+        className={`tab-pane ${selected ? "active" : ""}`}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({ tab: state.tab });
+export default connect(mapStateToProps)(TabContent);
