a41607923b8b0a5de870fc35e3b8cd9afb61ada0
thallysbraz <thallysbraz3@gmail.com>
2019-11-13 11:37:18 -0300

    melhorias na inclusão do Ciclo de Pagamento

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-
 import { toastr } from "react-redux-toastr";
+import { reset as resetForm } from "redux-form";
+import { showTabs, selectTab } from "../common/tab/tabActions";
 
 const BASE_URL = "http://localhost:3003/api";
 
@@ -14,15 +15,22 @@ export function getList() {
 }
 
 export function create(values) {
-  axios
-    .post(`${BASE_URL}/billingCycles`, values)
-    .then(resp => {
-      toastr.success("Sucesso", "Operação realizada com sucesso.");
-    })
-    .catch(error => {
-      error.response.data.errors.forEach(error => toastr.error("Error", error));
-    });
-  return {
-    type: "TEMP"
+  return dispatch => {
+    axios
+      .post(`${BASE_URL}/billingCycles`, values)
+      .then(resp => {
+        toastr.success("Sucesso", "Operação realizada com sucesso.");
+        dispatch([
+          resetForm("billingCycleForm"),
+          getList(),
+          selectTab("tabList"),
+          showTabs("tabList", "tabCreate")
+        ]);
+      })
+      .catch(error => {
+        error.response.data.errors.forEach(error =>
+          toastr.error("Error", error)
+        );
+      });
   };
 }
