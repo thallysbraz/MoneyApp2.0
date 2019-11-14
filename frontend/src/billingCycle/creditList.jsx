b8859e6e9fb102e7c24d99a707ba100b35a2e266
thallysbraz <thallysbraz3@gmail.com>
2019-11-14 14:27:00 -0300

    Adicionar e Clonar Créditos

--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
-import { Field } from "redux-form";
+import { Field, arrayInsert } from "redux-form";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
 import Grid from "../common/layout/grid";
 import Input from "../common/form/input";
 
 class CreditList extends Component {
+  add(index, item = {}) {
+    if (!this.props.readOnly) {
+      this.props.arrayInsert("billingCycleForm", "credits", index, item);
+    }
+  }
+
   renderRows() {
     const list = this.props.list || [];
     return list.map((item, index) => (
@@ -25,7 +33,22 @@ class CreditList extends Component {
             readOnly={this.props.readOnly}
           />
         </td>
-        <td></td>
+        <td>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => this.add(index + 1)}
+          >
+            <i className="fa fa-plus"></i>
+          </button>
+          <button
+            type="button"
+            className="btn btn-warning"
+            onClick={() => this.add(index + 1, item)}
+          >
+            <i className="fa fa-clone"></i>
+          </button>
+        </td>
       </tr>
     ));
   }
@@ -40,7 +63,7 @@ class CreditList extends Component {
               <tr>
                 <th>Nome</th>
                 <th>Valor</th>
-                <th>Ações</th>
+                <th className="table-actions">Ações</th>
               </tr>
             </thead>
             <tbody>{this.renderRows()}</tbody>
@@ -51,4 +74,7 @@ class CreditList extends Component {
   }
 }
 
-export default CreditList;
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ arrayInsert }, dispatch);
+
+export default connect(null, mapDispatchToProps)(CreditList);
