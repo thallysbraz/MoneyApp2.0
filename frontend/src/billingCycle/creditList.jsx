5130356df30de714f38d6ae99c93fe69d16245ae
thallysbraz <thallysbraz3@gmail.com>
2019-11-14 14:31:02 -0300

    REmover Creditos

--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Field, arrayInsert } from "redux-form";
+import { Field, arrayInsert, arrayRemove } from "redux-form";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 
@@ -10,6 +10,12 @@ class CreditList extends Component {
   add(index, item = {}) {
     if (!this.props.readOnly) {
       this.props.arrayInsert("billingCycleForm", "credits", index, item);
+    }
+  }
+
+  remove(index) {
+    if (!this.props.readOnly && this.props.list.length > 1) {
+      this.props.arrayRemove("billingCycleForm", "credits", index);
     }
   }
 
@@ -48,6 +54,13 @@ class CreditList extends Component {
           >
             <i className="fa fa-clone"></i>
           </button>
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={() => this.remove(index)}
+          >
+            <i className="fa fa-trash-o"></i>
+          </button>
         </td>
       </tr>
     ));
@@ -75,6 +88,6 @@ class CreditList extends Component {
 }
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ arrayInsert }, dispatch);
+  bindActionCreators({ arrayInsert, arrayRemove }, dispatch);
 
 export default connect(null, mapDispatchToProps)(CreditList);
